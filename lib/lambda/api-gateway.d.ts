e2fc7b6c20d4c9c3efe61c7e14ec05d946fd1b7c
Timo Benkhard <tcbenkhard@gmail.com>
2023-03-22 13:00:37 +0100

    Output to lib

--- lib/lambda/api-gateway.d.ts
+++ lib/lambda/api-gateway.d.ts
@@ -5,8 +5,8 @@ interface ApiGatewayHandlerOptions extends Omit<APIGatewayProxyResult, 'body'> {
 export declare abstract class ApiGatewayHandler<Request, Response> {
     private options;
     constructor(options: ApiGatewayHandlerOptions);
-    protected abstract parseEvent(event: APIGatewayProxyEvent, Context: Context): Promise<Request>;
-    protected abstract handleRequest(request: Request): Promise<Response>;
+    protected abstract parseEvent(event: APIGatewayProxyEvent, Context: Context): Request;
+    protected abstract handleRequest(request: Request): Response;
     handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>;
 }
 export {};
